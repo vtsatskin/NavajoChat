03584311f9c642696c619a96da3447e64ed28e1b
Valentin Tsatskin <val@valtsatskin.com>
2013-09-07 12:57:56 -0400

    message interception

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(function(){
-	console.log("it worked!");
-})
+	var messageBox = document.querySelector("[name=message_body]");
+	var handleKeyDown = function (e) {
+		var sendWithButton = document.querySelector("._1rh");
+		if(!sendWithButton && e.which == 13) {
+			var messageBox = document.querySelector("[name=message_body]");
+			messageBox.value = "a changed value";
+		}
+	}
+	messageBox.parentNode.addEventListener('keydown', handleKeyDown, true);
+});
